4154a40db2231d10ddd2c38bf3eb5b3996a99e19
SluBard <66221733+SluBard@users.noreply.github.com>
2024-10-21 18:35:24 -0500

    update
    update

--- murach/cpp/vs/20240930/ex_starts/ch11_ex2_word_counter/word_counter/main.cpp
+++ murach/cpp/vs/20240930/ex_starts/ch11_ex2_word_counter/word_counter/main.cpp
@@ -2,38 +2,42 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <map>
 
 using namespace std;
 
+struct FileNotFound {
+	string message;
+	string filename;
+};
+
 vector<string> load_words(string filename);
-map<string, int> get_word_count(const vector<string>& words);
 
 int main() {
 	cout << "The Word Counter program\n\n";
 
-	string filename = "dickens.txt";
-	auto words = load_words(filename);
+	string filename = "";
+	cout << "Enter a file name: ";
+	cin >> filename;
 
-	cout << words.size() << " WORDS: ";
-	for (string word : words) {
-		cout << word << ' ';
+	vector<string> words;
+	try {
+		words = load_words(filename);
+
+		cout << endl;
+		cout << words.size() << " WORDS: ";
+		for (string word : words) {
+			cout << word << ' ';
+		}
+		cout << endl << endl;
 	}
-	cout << endl << endl;
-
-	auto word_count = get_word_count(words);
-
-	cout << word_count.size() << " UNIQUE WORDS: ";
-	for (auto pair : word_count) {
-		cout << pair.first << ' ';
+	catch (const FileNotFound& e) {
+		cout << e.message << endl;
+		cout << "You entered: " << e.filename << ".\n";
+		cout << "Exiting program...";
 	}
-	cout << endl << endl;
-
-	cout << "COUNT PER WORD: ";
-	for (auto pair : word_count) {
-		cout << pair.first << '=' << pair.second << ' ';
+	catch (...) {
+		throw;
 	}
-	cout << endl << endl;
 }
 
 vector<string> load_words(string filename) {
@@ -59,21 +63,11 @@ vector<string> load_words(string filename) {
 			words.push_back(new_word);      // add word 
 		}
 		infile.close();
+		return words;
 	}
-	return words;
-}
-
-map<string, int> get_word_count(const vector<string>& words) {
-	map<string, int> word_count{};
-
-	for (string word : words) {
-		auto search = word_count.find(word);
-		if (search == word_count.end()) {
-			word_count[word] = 1;   // not found - add word with count of 1
-		}
-		else {
-			word_count[word] += 1;  // found - increment count for word
-		}
+	else {
+		FileNotFound e{ "File not found.", filename };
+		throw e;
 	}
-	return word_count;
+
 }
